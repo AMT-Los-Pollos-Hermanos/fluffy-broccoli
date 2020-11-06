55d527152d4eb89d5c74c8ea2969d9d0ca21ae78
Gil Balsiger <gil.balsiger@gmail.com>
2020-11-06 15:21:16 +0100

    Initial Commit

---
+++ src/test/java/ch/heigvd/gamify/GamifyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.heigvd.gamify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
