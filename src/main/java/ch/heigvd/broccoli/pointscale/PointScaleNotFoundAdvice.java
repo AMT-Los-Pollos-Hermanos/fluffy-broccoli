f8737d52a64c0e5b34a0db3f7d0c89f7ae8cf2e1
Julien Béguin <julien.beguin99@gmail.com>
2020-11-25 12:15:31 +0100

    add custom exception classes

---
+++ src/main/java/ch/heigvd/broccoli/pointscale/PointScaleNotFoundAdvice.java
@@ -0,0 +1,22 @@
+package ch.heigvd.broccoli.pointscale;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+class PointScaleNotFoundAdvice {
+
+    @ExceptionHandler(PointScaleNotFoundException.class)
+    ResponseEntity<?> badgeNotFoundHandler(PointScaleNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.TEXT_PLAIN).body(e.getMessage());
+    }
+
+    @ExceptionHandler(PointScaleNotAuthorizedException.class)
+    ResponseEntity<?> BadgeNotAuthorizedHandler(PointScaleNotAuthorizedException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).contentType(MediaType.TEXT_PLAIN).body(e.getMessage());
+    }
+
+}
