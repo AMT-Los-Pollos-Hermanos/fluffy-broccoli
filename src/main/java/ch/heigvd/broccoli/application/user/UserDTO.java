e1dbb72620e317a1db3fd538be79b959fe081e7b
Julien Béguin <julien.beguin99@gmail.com>
2020-12-11 17:14:00 +0100

    add todo

--- src/main/java/ch/heigvd/broccoli/application/user/UserDTO.java
+++ src/main/java/ch/heigvd/broccoli/application/user/UserDTO.java
@@ -3,19 +3,16 @@ package ch.heigvd.broccoli.application.user;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
 @Data
 @Builder
 public class UserDTO {
-    @Id
-    @GeneratedValue
-    private long id;
+    private long id; // TODO must be UUID or String
 
     private String firstname;
 
     private String lastname;
 
     private String username;
+
+    // TODO need List<BadgeDTO> and List<UserPointScaleDTO>
 }
