76946df8d355f896a670e34ca4d4a19c54bda4f0
Gil Balsiger <gil.balsiger@gmail.com>
2021-01-06 09:25:48 +0100

    test: refactor Cucumber step definitions

--- src/test/java/ch/heigvd/broccoli/StepDefinitions.java
+++ src/test/java/ch/heigvd/broccoli/StepDefinitions.java
@@ -1,26 +1,24 @@
-package ch.heigvd.broccoli.application.badge;
+package ch.heigvd.broccoli;
 
-import ch.heigvd.broccoli.SpringIntegrationTest;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.http.MediaType;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.io.UnsupportedEncodingException;
-import io.cucumber.spring.CucumberContextConfiguration;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -30,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(properties = {"spring.datasource.url=jdbc:h2:mem:db"})
 @AutoConfigureMockMvc
 @CucumberContextConfiguration
-public class BadgeStepDefinition {
+public class StepDefinitions {
 
     @Autowired
     private MockMvc mvc;
--- src/test/resources/rules.feature
+++
@@ -1,4 +0,0 @@
-Feature: Testing rules
-  Scenario: Get all rules
-    When the client get all rules
-    Then the client receives a 200
